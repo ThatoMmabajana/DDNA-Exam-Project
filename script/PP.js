40a484b3b3e5a9c7e79d11111fae795cadbaecaf
ThatoMmabajana <149573558+ThatoMmabajana@users.noreply.github.com>
2023-11-11 22:30:00 +0200

    Update PP.js

--- script/PP.js
+++ script/PP.js
@@ -6,6 +6,10 @@ function displaySocialsAlert(){
   alert("Explore my Social Media Profiles!")
 };
 
+function showUnderDevelopment(){
+  alert("This Section is under development.Please explore my social media profiles instead.")
+};
+
 // Alert message when clicking the "Read More" button
 document.querySelectorAll('.read-more-btn').forEach(function(button) {
   button.addEventListener('click', function() {

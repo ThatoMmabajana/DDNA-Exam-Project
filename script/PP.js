d28d3a9457b3af4e5ced9e9c39f5165e91a79b67
ThatoMmabajana <149573558+ThatoMmabajana@users.noreply.github.com>
2023-11-12 22:19:45 +0200

    Update PP.js

--- script/PP.js
+++ script/PP.js
@@ -1,16 +1,5 @@
-function contactPopUp(){
-  alert("Under Development!!")
-};
 
-function displaySocialsAlert(){
-  alert("Explore my Social Media Profiles!")
-};
 
-function showUnderDevelopmentAlert(){
-  alert("This Section is under development.Please explore my social media profiles instead.")
-};
-
-// Alert message when clicking the "Read More" button
 document.querySelectorAll('.read-more-btn').forEach(function(button) {
   button.addEventListener('click', function() {
       alert("This section is still under development!!.");

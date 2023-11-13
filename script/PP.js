5e599546599c1e7fa8f11d4f17d02041600c911e
Thato Tshego <149573558+ThatoMmabajana@users.noreply.github.com>
2023-11-13 08:04:26 +0200

    Update PP.js

--- script/PP.js
+++ script/PP.js
@@ -2,6 +2,6 @@
 
 document.querySelectorAll('.read-more-btn').forEach(function(button) {
   button.addEventListener('click', function() {
-      alert("This section is still under development!!.Kindly explore my social profiles instead");
+      alert("This section is still under development!!.Kindly explore my social profiles instead.");
   });
 });
